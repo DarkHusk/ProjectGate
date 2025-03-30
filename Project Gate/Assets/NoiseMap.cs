aa97b411b1616cd0bf21f9773918f00a27b01032
justynapiotrowska <tusiaczek57@wp.pl>
2025-03-30 12:50:54 +0200

    Added seed to have different outputs, improved terain shaping

--- Project Gate/Assets/NoiseMap.cs
+++ Project Gate/Assets/NoiseMap.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UIElements;
+using NUnit.Framework;
 public class NoiseMap
 {
     float[,] Map;
-    public void GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float lacunarity, float persistance)  
+    public void GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float lacunarity, float persistance)  
     {
+        Assert.IsTrue(mapWidth > 0 && mapHeight > 0 && octaves >=0);
+        System.Random offset = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        int offsetMin = -100000, offsetMax = 100000;
+        for (int i = 0; i < octaves; i++)
+        {
+            octaveOffsets[i] = new Vector2(offset.Next(offsetMin, offsetMax), offset.Next(offsetMin, offsetMax));
+        }
         Map = new float[mapWidth, mapHeight];
         float amplitude = 1;
         float frequency = 1;
@@ -21,7 +30,8 @@ public class NoiseMap
                 frequency = 1;
                 for(int i =0; i< octaves; i++)
                 {
-                    noise += Mathf.PerlinNoise(x / scale * frequency, y / scale * frequency)*amplitude;
+                    noise += Mathf.PerlinNoise(x / scale * frequency + octaveOffsets[i].x, 
+                        y / scale * frequency + octaveOffsets[i].y) * amplitude;
 
                     amplitude *= persistance;
                     frequency *= lacunarity;
--- Project Gate/Assets/TerrainGenerator.cs
+++ Project Gate/Assets/TerrainGenerator.cs
@@ -6,9 +6,10 @@ public class TerrainGenerator : MonoBehaviour
     public Terrain terrain;
     public float scale = 1;
     public int octaves = 1;
-    public float persistance = 0; //decrease in amplitude of octaves
+    [Range(0,1)]
+    public float persistance = 0; //decrease in amplitude of octaves, only in range <0,1>
     public float lacunarity = 0; //increse in frequency of octaves
-
+    public int seed = 0;
     void Start()
     {
         generateTerrain();
@@ -19,7 +20,7 @@ public class TerrainGenerator : MonoBehaviour
     {
         int res = this.terrain.terrainData.heightmapResolution;
         NoiseMap map = new NoiseMap();
-        map.GenerateNoiseMap(res, res, scale, octaves, lacunarity, persistance);
+        map.GenerateNoiseMap(res, res, seed, scale, octaves, lacunarity, persistance);
         this.terrain.terrainData.SetHeights(0, 0, map.getNoiseMap());
     }
 
